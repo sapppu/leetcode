de3d06c43ad0f612bc9b954a1ba05c234aa8b177
Swapnaj Santosh Tolnure <146807393+sapppu@users.noreply.github.com>
2026-08-02 20:39:58 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.34%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+
+        while (x != 0) {
+            int d = x % 10;
+            x /= 10;
+
+            if (ans > 214748364 || (ans == 214748364 && d > 7))
+                return 0;
+
+            if (ans < -214748364 || (ans == -214748364 && d < -8))
+                return 0;
+
+            ans = ans * 10 + d;
+        }
+
+        return ans;
+    }
+};
