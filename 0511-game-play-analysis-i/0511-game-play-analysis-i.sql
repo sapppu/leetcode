907855778aad161aaefd76fa13d7b063ca9e801d
Swapnaj Santosh Tolnure <146807393+sapppu@users.noreply.github.com>
2026-08-16 19:13:12 +0530

    Time: 725 ms (9.11%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select distinct player_id, min(event_date) over (partition by player_id) as first_login from activity;
+select a.player_id,a.event_date first_login from activity a where not exists(select 1 from activity b where b.player_id=a.player_id and b.event_date<a.event_date);
